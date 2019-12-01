63cd0c678de57bf2911ab1ca114902c73d3f80aa
Cirrus CI <support@cirruslabs.org>
2019-12-01 23:17:17 +0000

    add grid of cells

--- lib/message.dart
+++ lib/message.dart
@@ -2,45 +2,49 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 class Message extends StatefulWidget {
-    Message({Key key}) : super(key: key);
+  Message({Key key}) : super(key: key);
 
-    @override
-    MessageState createState() => new MessageState();
+  @override
+  MessageState createState() => MessageState();
 }
 
 class MessageState extends State<Message> {
-    final messages = [
-        'No, you cannot.',
-        'Foxo wins again!',
-        'Too crafty for you.',
-        'Foxtastic!',
-        'What does the foxo say?',
-        'Better luck next time.',
-        'Foxo remains undefeated.',
-        'Foxo always wins!',
-    ];
-    String message;
+  final messages = [
+      'No, you cannot.',
+      'Foxo wins again!',
+      'Is he too crafty for you?',
+      'How Foxtastic!',
+      'What does the foxo say?',
+      'Better luck next time.',
+      'Foxo remains undefeated.',
+      'Foxo always wins!',
+      'Minecraft has foxes too!',
+  ];
+  String message;
 
-    MessageState() {
-        final random = new Random();
-        final index = random.nextInt(this.messages.length);
-        message = messages[index];
-    }
+  MessageState() {
+    final random = Random();
+    final index = random.nextInt(this.messages.length);
+    message = messages[index];
+  }
 
-    @override
-    Widget build(BuildContext context) {
-        return new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-                new Text(
-                    'Can you outsmart foxo?',
-                    style: Theme.of(context).textTheme.title,
-                ),
-                new Text(
-                    message,
-                    style: Theme.of(context).textTheme.subhead,
-                ),
-            ],
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          child: Text(
+              'Can you outsmart foxo?',
+              style: Theme.of(context).textTheme.title,
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 10),
+        ),
+        Text(
+          message,
+          style: Theme.of(context).textTheme.subhead,
+        ),
+      ],
+    );
+  }
 }
