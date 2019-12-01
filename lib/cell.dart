4b29893ec3b6d4be5d5325cf3dea773a5c55db21
Cirrus CI <support@cirruslabs.org>
2019-12-01 22:31:33 +0000

    update flutter in travis

--- lib/cell.dart
+++ lib/cell.dart
@@ -2,22 +2,6 @@ import 'package:flutter/material.dart';
 
 class Cell extends StatelessWidget {
     @override Widget build(BuildContext context) {
-        return new Container(
-            decoration: new BoxDecoration(
-                borderRadius: new BorderRadius.all(
-                    Radius.circular(10.0),
-                ),
-                boxShadow: [
-                    new BoxShadow(
-                        color: Colors.grey[400],
-                        offset: new Offset(5.0, 5.0),
-                        blurRadius: 5.0,
-                    )
-                ],
-                color: Colors.grey[300],
-            ),
-            height: 90.0,
-            width: 90.0,
-        );
+        return new Image(image: AssetImage('images/cell.png'));
     }
 }
