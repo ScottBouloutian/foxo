93ddbaeb0ee273c5d622036a57266a099b1c3138
Cirrus CI <support@cirruslabs.org>
2019-12-02 00:12:01 +0000

    add tap gesture detector

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,39 +3,44 @@ import 'message.dart';
 import 'cell.dart';
 
 class HomePage extends StatefulWidget {
-    HomePage({Key key, this.title}) : super(key: key);
+  HomePage({Key key, this.title}) : super(key: key);
 
-    final String title;
+  final String title;
 
-    @override
-    _HomePageState createState() => _HomePageState();
+  @override
+  _HomePageState createState() => _HomePageState();
 }
 
 // Home Screen State
 class _HomePageState extends State<HomePage> {
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(
-                title: Text(widget.title),
-            ),
-            body: Column(
-                children: [
-                    Message(),
-                    Expanded(
-                      child: GridView.count(
-                          crossAxisCount: 3,
-                          children: List.generate(9, (index) {
-                              return Container(
-                                  child: Cell(),
-                                  padding: const EdgeInsets.all(10),
-                              );
-                          }),
-                          padding: const EdgeInsets.all(20),
-                      )
-                    )
-                ],
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          Message(),
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 3,
+              children: List.generate(9, (index) {
+                return Container(
+                  child: GestureDetector(
+                    onTap: () {
+                      print('onTap called. $index');
+                    },
+                    child: Cell(),
+                  ),
+                  padding: const EdgeInsets.all(10),
+                );
+              }),
+              padding: const EdgeInsets.all(20),
             )
-        );
-    }
+          )
+        ],
+      )
+    );
+  }
 }
