cbd1fff094df2ed7daf233884f770edc3468f1c2
Cirrus CI <support@cirruslabs.org>
2019-12-11 21:00:19 +0000

    move game board to its own class

--- lib/message.dart
+++ lib/message.dart
@@ -10,15 +10,15 @@ class Message extends StatefulWidget {
 
 class MessageState extends State<Message> {
   final messages = [
-      'No, you cannot.',
-      'Foxo wins again!',
-      'Is he too crafty for you?',
-      'How Foxtastic!',
-      'What does the foxo say?',
-      'Better luck next time.',
-      'Foxo remains undefeated.',
-      'Foxo always wins!',
-      'Minecraft has foxes too!',
+    'No, you cannot.',
+    'Foxo wins again!',
+    'Is he too crafty for you?',
+    'How Foxtastic!',
+    'What does the foxo say?',
+    'Better luck next time.',
+    'Foxo remains undefeated.',
+    'Foxo always wins!',
+    'Minecraft has foxes too!',
   ];
   String message;
 
@@ -35,8 +35,8 @@ class MessageState extends State<Message> {
       children: [
         Container(
           child: Text(
-              'Can you outsmart foxo?',
-              style: Theme.of(context).textTheme.title,
+            'Can you outsmart foxo?',
+            style: Theme.of(context).textTheme.title,
           ),
           padding: const EdgeInsets.symmetric(vertical: 10),
         ),
