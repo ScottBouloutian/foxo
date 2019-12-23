76b207b128dabe0f111d1fa776c0d962865d5bd8
Cirrus CI <support@cirruslabs.org>
2019-12-23 02:18:45 +0000

    add cell animations

--- lib/cell.dart
+++ lib/cell.dart
@@ -2,18 +2,23 @@ import 'package:flutter/material.dart';
 import 'enums.dart';
 
 class Cell extends StatefulWidget {
-  Cell({
-    Key key,
-    this.type = CellType.empty,
-  }) : super(key: key);
-
   final CellType type;
 
+  Cell({
+    Key key,
+    this.type,
+  }) : super(key: key);
+
   @override
-  _CellState createState() => _CellState();
+  CellState createState() => CellState();
 }
 
-class _CellState extends State<Cell> {
+class CellState extends State<Cell> with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> animation;
+  Tween<double> turnsTween;
+  Tween<double> scaleTween;
+
   Widget buildImage() {
     switch (widget.type) {
       case CellType.empty:
@@ -22,12 +27,50 @@ class _CellState extends State<Cell> {
         return Image.asset('images/foxo.png');
       case CellType.chick:
         return Image.asset('images/chick.png');
+      default:
+        return null;
     }
   }
 
-  @override Widget build(BuildContext context) {
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+    animation  = CurvedAnimation(
+      parent: controller,
+      curve: Curves.easeOutBack,
+    );
+    turnsTween = Tween<double>(
+      begin: 0,
+      end: 1,
+    );
+    scaleTween = Tween<double>(
+      begin: 0.5,
+      end: 1,
+    );
+  }
+
+  @override
+  didUpdateWidget(oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.type == CellType.empty && widget.type != CellType.empty) {
+      controller.forward();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      child: this.buildImage(),
+      child: RotationTransition(
+        child: ScaleTransition(
+          child: this.buildImage(),
+          scale: scaleTween.animate(animation),
+        ),
+        turns: turnsTween.animate(animation),
+      ),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.all(
           Radius.circular(10),
