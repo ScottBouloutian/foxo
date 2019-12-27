be46ee25d2456bbf5613d0d9cb3fa86ce00c35b4
Cirrus CI <support@cirruslabs.org>
2019-12-27 23:50:39 +0000

    add core game logic

--- lib/app.dart
+++ lib/app.dart
@@ -2,27 +2,27 @@ import 'package:flutter/material.dart';
 import 'home_page.dart';
 
 class App extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-        return MaterialApp(
-            title: 'Foxo',
-            theme: ThemeData(
-                primarySwatch: Colors.orange,
-                primaryTextTheme: TextTheme(
-                    title: TextStyle(
-                        color: Colors.white,
-                    ),
-                ),
-                textTheme: TextTheme(
-                    title: TextStyle(
-                        color: Colors.black,
-                    ),
-                    subhead: TextStyle(
-                        color: Colors.green,
-                    ),
-                ),
-            ),
-            home: HomePage(title: 'Foxo'),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Foxo',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        primaryTextTheme: TextTheme(
+          title: TextStyle(
+              color: Colors.white,
+          ),
+        ),
+        textTheme: TextTheme(
+          title: TextStyle(
+              color: Colors.black,
+          ),
+          subhead: TextStyle(
+              color: Colors.green,
+          ),
+        ),
+      ),
+      home: HomePage(title: 'Foxo'),
+    );
+  }
 }
